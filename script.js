309fdd9e5118b596c356a5a6c166974e6a367768
debor4230908 <debora.rodrigues.araujo@escola.pr.gov.br>
2025-08-20 11:47:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const perguntas = [
       alternativas: [
           {
               texto:"Isso é assustador!",
-              afirmacao: "afirmacao"
+              afirmacao: "Os robôs irão destruir a humanidade!!!"
           },
           {
               texto:"Isso é maravilhoso!",
-              afirmacao: "afirmacao"
+              afirmacao: "Irá me ajudar em diversos trabalhos escolares."
           }  
       ],
     },
@@ -38,11 +38,11 @@ const perguntas = [
       alternativas: [
           {
               texto:  "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-              afirmacao: "afirmacao"
+              afirmacao: "A IA pode ser uma grande aliada para resolver questões que levaria dias, meses ou anos para ser resolvida."
           },
           {
               texto:  "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores.",
-              afirmacao: "afirmacao"
+              afirmacao: "A IA é usada por pessoas preguiçosas, ela está presente apenas para regredir a inteligência e a capacidade dos seres humanos."
           }    
       ],
     },
@@ -52,11 +52,11 @@ const perguntas = [
       alternativas: [
           {
               texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-              afirmacao: "afirmacao"
+              afirmacao: "Usar minha criatividade e usar formas e cores retiradas da minha cabeça para demonstrar meus sentimentos."
           },
           {
               texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-              afirmacao: "afirmacao"
+              afirmacao: "Utilizar meus conhecimentos técnicos e desenvolver comando para a IA desenvolver uma imagina da forma exata em que eu quero."
           }    
       ],
     },
@@ -66,11 +66,11 @@ const perguntas = [
       alternativas: [
           {
               texto:  "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-              afirmacao: "afirmacao"
+              afirmacao: "Realmente, usar a IA em casos extremos pode ser necessário, e não é um problema ela estar presente em seu trabalho."
           },
           {
               texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-              afirmacao: "afirmacao"
+              afirmacao: "Não acho de bom tom usar a IA para fazer o trabalho completo. e sim para auxiliar."
           }    
       ]
     },
